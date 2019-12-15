5f8cef0c2d8e4662affa7c010a8ab4aaa3f2a599
Coloryr <402067010@qq.com>
2019-12-15 17:18:55 +0800

    finish

---
+++ src/main/java/Color_yr/all_music_mod/all_music_mod.java
@@ -0,0 +1,139 @@
+package Color_yr.all_music_mod;
+
+import io.netty.buffer.ByteBuf;
+import javazoom.jl.player.Player;
+import net.minecraft.network.NetworkSystem;
+import net.minecraft.network.PacketBuffer;
+import net.minecraft.network.play.client.CCustomPayloadPacket;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.event.ClientPlayerNetworkEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.network.FMLMCRegisterPacketHandler;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import net.minecraftforge.fml.network.NetworkEvent;
+import net.minecraftforge.fml.network.NetworkRegistry;
+import net.minecraftforge.fml.network.event.EventNetworkChannel;
+import net.minecraftforge.fml.network.simple.SimpleChannel;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.FloatControl;
+import javax.sound.sampled.Mixer;
+import javax.sound.sampled.Port;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.function.Supplier;
+
+@Mod("all_music_mod")
+public class all_music_mod {
+    private static final Logger LOGGER = LogManager.getLogger();
+    private static Player nowPlaying;
+    private SimpleChannel channel;
+    private static URL nowURL;
+
+    public all_music_mod() {
+        // Register the setup method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        // Register the enqueueIMC method for modloading
+        //FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
+        // Register the processIMC method for modloading
+        //FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
+        // Register the doClientStuff method for modloading
+        //FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLClientSetupEvent event) {
+        MinecraftForge.EVENT_BUS.register(this);
+        channel = NetworkRegistry.ChannelBuilder
+                .named(new ResourceLocation("allmusic", "channel"))
+                .networkProtocolVersion(() -> "zzzz")
+                .serverAcceptedVersions(NetworkRegistry.ACCEPTVANILLA::equals)
+                .clientAcceptedVersions(NetworkRegistry.ACCEPTVANILLA::equals)
+                .simpleChannel();
+        channel.registerMessage(666, String.class, this::enc, this::dec, this::proc);
+        set(100);
+    }
+    private void enc(String str, PacketBuffer buffer) {
+        buffer.writeBytes(str.getBytes(StandardCharsets.UTF_8));
+    }
+
+    private String dec(PacketBuffer buffer) {
+        return buffer.toString(StandardCharsets.UTF_8);
+    }
+    private void proc(String str, Supplier<NetworkEvent.Context> supplier) {
+        onClicentPacket(str);
+        NetworkEvent.Context context = supplier.get();
+        context.setPacketHandled(true);
+    }
+
+    @SubscribeEvent
+    public void onServerQuit(final ClientPlayerNetworkEvent.LoggedOutEvent e) {
+        if (all_music_mod.nowPlaying != null && !all_music_mod.nowPlaying.isComplete()) {
+            all_music_mod.nowPlaying.close();
+        }
+    }
+
+    private void onClicentPacket(final String message) {
+        final Thread asyncThread = new Thread(() -> {
+            if (message.equals("[Stop]")) {
+                all_music_mod.this.stopPlaying();
+            } else if (message.startsWith("[Play]")) {
+                try {
+                    if (nowPlaying != null && !nowPlaying.isComplete()) {
+                        nowPlaying.close();
+                    }
+                    all_music_mod.nowURL = new URL(message.replace("[Play]", ""));
+                    nowPlaying = new Player(nowURL.openStream());
+                    nowPlaying.play();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else if (message.startsWith("[V]")) {
+                try {
+                    String a = message.replace("[V]", "");
+                    set(Integer.parseInt(a));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        asyncThread.start();
+    }
+
+    private void stopPlaying() {
+        if (nowPlaying != null) {
+            nowPlaying.close();
+            nowPlaying = null;
+        }
+    }
+
+    private void set(int a) {
+        try {
+            final Mixer.Info[] mixerInfo = AudioSystem.getMixerInfo();
+            for (final Mixer.Info info : mixerInfo) {
+                final Mixer mixer = AudioSystem.getMixer(info);
+                if (mixer.isLineSupported(Port.Info.SPEAKER)) {
+                    final Port port = (Port) mixer.getLine(Port.Info.SPEAKER);
+                    port.open();
+                    if (port.isControlSupported(FloatControl.Type.VOLUME)) {
+                        final FloatControl volume = (FloatControl) port.getControl(FloatControl.Type.VOLUME);
+                        volume.setValue((float) a / 1000);
+                    }
+                    port.close();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
