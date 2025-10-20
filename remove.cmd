65644c90613a0124cc8100e3bc1ea3cb46a5d5ce
baiyuxiang12 <baiyuxiang12@users.noreply.github.com>
2025-10-20 00:18:34 -0400

    按要求删除了core符号链接,更新了link.cmd、remove.cmd和github action配置

--- remove.cmd
+++ remove.cmd
@@ -7,7 +7,7 @@ fabric_1_19 fabric_1_19_3 fabric_1_19_4 fabric_1_20_1 fabric_1_20_6 fabric_1_21 
 forge_1_7_10 forge_1_12_2 forge_1_14_4 forge_1_15_2 forge_1_16_5 ^
 forge_1_17_1 forge_1_18_2 forge_1_19_2 forge_1_19_3 forge_1_20_1 ^
 forge_1_20_4 forge_1_20_6 forge_1_21 neoforge_1_20_4 neoforge_1_20_6 neoforge_1_21 ^
-neoforge_1_21_3 fabric_1_21_3 neoforge_1_21_5 fabric_1_21_5 fabric_1_21_6 neoforge_1_21_6
+neoforge_1_21_3 fabric_1_21_3 neoforge_1_21_5 fabric_1_21_5 fabric_1_21_6 neoforge_1_21_6 fabric_1_21_8
 
 :: 遍历数组
 for %%a in (%array%) do (
