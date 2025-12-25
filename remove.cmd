b192c79cc150c396a92a6384f161f13cc47bca99
Color_yr <402067010@qq.com>
2025-12-25 13:41:24 +0800

    move

--- remove.cmd
+++ remove.cmd
@@ -2,13 +2,10 @@
 setlocal enabledelayedexpansion
 
 :: 定义数组元素
-set array=fabric_1_14_4 fabric_1_15_2 fabric_1_16_5 fabric_1_17_1 fabric_1_18_2 ^
-fabric_1_19 fabric_1_19_3 fabric_1_19_4 fabric_1_20_1 fabric_1_20_6 fabric_1_21 ^
+set array=fabric_1_16_5 fabric_1_20_1 fabric_1_21 fabric_1_21_6 ^
 fabric_26_1 ^
-forge_1_7_10 forge_1_12_2 forge_1_14_4 forge_1_15_2 forge_1_16_5 ^
-forge_1_17_1 forge_1_18_2 forge_1_19_2 forge_1_19_3 forge_1_20_1 ^
-forge_1_20_4 forge_1_20_6 forge_1_21 neoforge_1_20_4 neoforge_1_20_6 neoforge_1_21 ^
-neoforge_1_21_3 fabric_1_21_3 neoforge_1_21_5 fabric_1_21_5 fabric_1_21_6 neoforge_1_21_6
+forge_1_7_10 forge_1_12_2 forge_1_16_5 forge_1_20_1 ^
+neoforge_1_21 neoforge_1_21_6
 
 :: 遍历数组
 for %%a in (%array%) do (
